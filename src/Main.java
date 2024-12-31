8e40f0a0356c2b9a71ce6fc9f7ee9579ee6bfaeb
Mohamed Dawy <mohamedahmeddawy0@gmail.com>
2024-12-31 21:55:59 +0200

    Two methods have been created in staff class for 2 employees

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main (String [] args){
+        Staff emplpyee1= new Staff();
+        emplpyee1.name = "Ahmed";
+        emplpyee1.staffID= 12345;
+        emplpyee1.salary=1.500;
+        emplpyee1.printStaffData();
 
+        Staff emplpyee2= new Staff();
+        emplpyee2.name = "Ali";
+        emplpyee2.staffID= 123;
+        emplpyee2.salary=2.500;
+        emplpyee2.printStaffData();
     }
 }
--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Staff {
+
+    String name;
+    int staffID;
+    double salary;
+
+    public void printStaffData(){
+        System.out.println( "Name of the first employee: " + name);
+        System.out.println( "Employee ID: " + staffID);
+        System.out.println( "Salary: " + salary);
+
+    }
+
+
 }
